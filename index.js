71120a5bca6a331e242d6b83bbd96eb7ff803814
WAHU0508 <sharongikenye@gmail.com>
2024-06-27 12:16:06 +0300

    Add functions in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+//function declaration
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+console.log(saturdayFun());
+console.log(saturdayFun('swim'));
+
+//function expression
+const mondayWork = function(activity = "go to the office") { 
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork());
+console.log(mondayWork('work from home'));
+
+//function-level scope
+function wrapAdjective(value = "*") {
+    function withValue(input = "special") {
+        return `You are ${value}${input}${value}!`;
+    }
+    return withValue;
+}
+console.log(wrapAdjective("%")("a dedicated programmer"));
